2b5eec6037608a5211e4affa9fff91441cc2bfb1
Elukoye <lukoyedith@gmail.com>
2020-12-21 20:45:43 +0300

    add slack_adderbot folder and file

---
+++ slack_adderbot/commands/calculate.rb
@@ -0,0 +1,9 @@
+module AdderBot
+  module commands
+    class Calculate < SlackRubyBot::Commands::Base
+      command 'calculate' do |client, data, _match|
+        client.say(channel: data.channel, text:'4')
+      end
+    end
+  end
+end
