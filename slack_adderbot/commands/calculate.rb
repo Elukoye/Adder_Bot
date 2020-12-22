0f79a7daf1bd8b880d409e4df47e376483969285
Elukoye <lukoyedith@gmail.com>
2020-12-22 16:03:16 +0300

    modify calculate.rb class method

--- slack_adderbot/commands/calculate.rb
+++ slack_adderbot/commands/calculate.rb
@@ -2,8 +2,8 @@ module AdderBot
   module Commands
     class Calculate < SlackRubyBot::Commands::Base
       command 'calculate' do |client, data, _match|
-        client.say(channel: data.channel, text:'4')
-      end
+        client.say(channel: data.channel, text: '4')
+       end
     end
   end
 end
